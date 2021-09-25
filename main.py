7d6a33c2c4ccae32c95007db7c196a8eb410ab36
Guto Xavier <gutoffline@gmail.com>
2021-09-25 11:48:40 -0300

    Primeira parte da aula 3

---
+++ main.py
@@ -0,0 +1,33 @@
+# import this
+
+#operadores : + - / *
+print(2 ** 4) # potência
+print(10/3)
+print(10 % 3)
+print(10//3)
+
+#help()
+print(type(10))
+print(type(10.0))
+print(type('10'))
+print(type(True))
+print(type(False))
+
+nome = "João Paulo"
+print(type(nome))
+#tipagem dinâmica -> a variável assume o tipo dependendo da informaçõa
+
+print(nome)
+print(nome[3])
+print(nome[0:4])
+print(nome[2:])
+print(nome[:3])
+print(len(nome))
+print(nome.lower())
+print(nome.upper())
+print(nome.capitalize())
+print("J" in nome)
+print(nome.count("a") + nome.count("ã"))
+print(nome.startswith("J"))
+print(nome.endswith("o"))
+
